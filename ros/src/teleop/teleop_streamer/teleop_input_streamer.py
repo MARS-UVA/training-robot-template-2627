a37f3d9fe7e66443977936f7c4d018cc739f6c0e
dylann123 <dylannguyen0529@gmail.com>
2026-09-10 00:19:20 -0400

    need keep alive for robot action persistence

--- ros/src/teleop/teleop_streamer/teleop_input_streamer.py
+++ ros/src/teleop/teleop_streamer/teleop_input_streamer.py
@@ -1,8 +1,8 @@
+from threading import Thread
 from rclpy.node import Node
 from teleop_msgs.msg import GamepadState
 from geometry_msgs.msg import Twist
 from rclpy.callback_groups import ReentrantCallbackGroup
-
 
 class TeleopInputStreamer(Node):
 
@@ -10,7 +10,7 @@ class TeleopInputStreamer(Node):
         super().__init__("teleop_input_streamer")
 
         # allow multiple streams (not sure how feasible this is, but feels intuitive)
-        STREAM_CALLBACK_GROUP = ReentrantCallbackGroup()
+        self.STREAM_CALLBACK_GROUP = ReentrantCallbackGroup()
 
         self.twist_publisher = self.create_publisher(Twist, "/teleop/cmd_vel", 10)
 
@@ -19,11 +19,13 @@ class TeleopInputStreamer(Node):
             msg_type=GamepadState,
             callback=self.teleop_callback,
             qos_profile=10,
-            callback_group=STREAM_CALLBACK_GROUP,
+            callback_group=self.STREAM_CALLBACK_GROUP,
         )
+        
+        self.current_twist = Twist()
+        self.start_keep_alive()
 
     def teleop_callback(self, gamepad_input: GamepadState):
-        
         # x will be our motor offset scalar, [-1, 1]
         # y will be our motor strength scalar, [-1, 1]
         direction = (gamepad_input.left_stick.x, gamepad_input.left_stick.y)
@@ -34,4 +36,25 @@ class TeleopInputStreamer(Node):
         twist.angular.z = direction[1]
         
         self.twist_publisher.publish(twist)
-        pass
+        self.current_twist = twist
+        
+    # the board disengages motors after no input is recieved in ~1s
+    # we should replay inputs every 0.5s to keep unchanging inputs live
+    def start_keep_alive(self):
+        KEEP_ALIVE_RATE = self.create_rate(2.0)
+        
+        def keep_alive():
+            previous_twist = None
+            
+            while True:
+                is_twist_empty = self.current_twist == None or self.current_twist == Twist()
+                if not is_twist_empty:
+                    if self.current_twist != previous_twist:
+                        previous_twist = self.current_twist
+                    else:
+                        self.twist_publisher.publish(self.current_twist)
+                
+                KEEP_ALIVE_RATE.sleep()
+        
+        # thread to avoid halting
+        Thread(target=keep_alive).start()
