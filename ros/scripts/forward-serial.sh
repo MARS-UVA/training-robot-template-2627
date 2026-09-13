c48a5e07a578891ce99c53f0e3445ce94d2f2d36
Anthony Vu <anthonythongvu@gmail.com>
2026-09-13 15:42:18 +0000

    update forward serial script

--- ros/scripts/forward-serial.sh
+++ ros/scripts/forward-serial.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo socat PTY,link=/dev/ttyUSB0,raw,echo=0,b115200,perm=0777 TCP:localhost:7777
+sudo socat PTY,link=/dev/ttyUSB0,raw,echo=0,b115200,perm=0777 TCP:host.docker.internal:7777
